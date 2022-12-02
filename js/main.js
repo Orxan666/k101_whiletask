35c33671050da5e6346393d37c059b23da9e43d2
orxan666 <orxanallahyarov03@gmail.com>
2022-12-02 12:48:39 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+// let myArr = ['cup', 'plate', 'spoon', 'knife', 'fork']
+
+// let i = 0
+
+// while (i < myArr.length) {
+//     console.log(myArr[i]);
+//     i+=1
+// }
+// -----------------------
+
+// let myArr = ['xiyar', 'pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni']
+
+// let i = 0
+
+// while (i < myArr.length) {
+//     let element = myArr[i]
+//     i++
+//     if (element[0] === "s") {
+//         console.log(element);
+//     }
+// }
+
+// let total = 0
+
+// let index = 1
+
+// while (index <= 100) {
+//     total += index
+
+//     index++
+// }
+// console.log(total); 
+
+// const myArr = [
+//     "kitab",
+//     "defter",
+//     "qelem",
+//     "karandas",
+//     "jurnal",
+//     "pozan",
+//     "vereq",
+// ];
+
+// let index = 0
+
+
+// while (index < myArr.length) {
+//     let element = myArr[index]
+//     index++
+//     if (myArr[index].length !== 5) {
+//         continue;
+//     }
+//     let netice = element[element.length -5]
+//     console.log(netice);
+
+// }
+
+
+// let number=100000
+
+// while(number>0){
+//     if(number%9999===0){
+//         console.log(number);
+//         break;
+
+//     }
+//     number--
+// }
+
+// let sentence = "Mən hər gün javascript öyrənirəm"
+
+// let saitler = "aıoueəiöü"
+
+// let total = ""
+// let i = 0
+
+
+// while (i < sentence.length) {
+//     const letter = sentence[i];
+//     if (!saitler.includes(letter)) {
+//         total += letter
+//     }
+//     i++
+// }
+
+// console.log(total);
+
+// let i = 0
+
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i += 1
+// }
+
+
+// for (let i=0;i<myArray.length;i+=1){
+//     console.log(myArray[i]);
+// }
+
+// let myArray = ["Lale", "Ayten", "Gulbadam"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log("Sennen zehlem gedir" + " " + myArray[i]);
+// }
